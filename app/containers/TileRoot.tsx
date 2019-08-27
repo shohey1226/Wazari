42bed29f0a9b644082fc7e1003b79ec2fde02c2f
Shohei Kameda <shoheik@cpan.org>
2019-08-27 11:53:02 +0900

    added refactored removePane

--- app/containers/TileRoot.tsx
+++ app/containers/TileRoot.tsx
@@ -19,7 +19,7 @@ import Browser from "./Browser";
 import { WebView } from "react-native-webview";
 
 interface State {
-  ids: Array<number>
+  ids: Array<number>;
 }
 
 interface Props {}
@@ -29,23 +29,24 @@ class TileRoot extends Component<Props, State> {
     super(props);
     let tree = new TreeModel();
     let root = tree.parse({
-      id: 1,
+      id: 1
     });
     console.log(root);
-    this.root = root;    
+    this.root = root;
 
-    let newNodeId = this.addPane("Row", 1)
-    console.log(this.root)
-    let newNodeId2 = this.addPane("Col", newNodeId)
-    console.log(this.root)
+    let newNodeId = this.addPane("Row", 1);
+    console.log(this.root);
+    // let newNodeId2 = this.addPane("Col", newNodeId);
+    // console.log(this.root);
 
- 
     this.state = {
-      ids: [1, newNodeId, newNodeId2 ]      
-    }
+      //ids: [1, newNodeId, newNodeId2]
+      ids: [1, newNodeId]
+    };
 
+    this.removePane(newNodeId);
+    console.log(this.root);
 
- 
     // console.log(root);
     // root.addChild(tree.parse({ id: "1.a", size: 50 }));
     // //root.addChild(tree.parse({ id: "1.b" }));
@@ -58,31 +59,27 @@ class TileRoot extends Component<Props, State> {
     // // root.addChild(n3);
     // // n3.addChild(tree.parse({ id: "44444444444444444" }));
     // // n3.addChild(tree.parse({ id: "55555555555555555" }));
-
   }
 
   componentDidMount() {
-    setTimeout(()=>{
-      this.removePane(1);
-      this.setState({ids: this.state.ids.filter(i=> i !== 1)})
-    }, 3000)
-
+    // setTimeout(() => {
+    //   this.removePane(1);
+    //   this.setState({ ids: this.state.ids.filter(i => i !== 1) });
+    // }, 3000);
   }
 
-  addPane(type: "Row" | "Col", targetPaneId: string): number|null {
-    let targetNode = this.root.first(node => {
-      if (node.model.id === targetPaneId) {
-        return true;
-      }
-    });
-    if(!targetNode){return null}
+  addPane(type: "Row" | "Col", targetPaneId: string): number | null {
+    let targetNode = this._findNode(targetPaneId);
 
+    if (!targetNode) {
+      return null;
+    }
 
     let tree = new TreeModel();
     // make current node branch and create a new node with targetNode.id
-    targetNode.model.type = type;    
-    targetNode.addChild(tree.parse({ id: targetNode.model.id}));          
-    targetNode.model.id = 'branch'        
+    targetNode.model.type = type;
+    targetNode.addChild(tree.parse({ id: targetNode.model.id }));
+    targetNode.model.id = "branch";
     // add new Node
     let id = Date.now();
     targetNode.addChild(tree.parse({ id: id }));
@@ -90,18 +87,43 @@ class TileRoot extends Component<Props, State> {
     return id;
   }
 
-  removePane(targetPaneId){
+  removePane(targetPaneId) {
     let targetNode = this._findNode(targetPaneId);
-    targetNode.drop();
+    let theOtherNode = targetNode.parent.children.filter(
+      n => n.model.id !== targetPaneId
+    )[0];
+
+    // if it's branch, then move up the node
+    if (theOtherNode.model.id === "branch") {
+      theOtherNode.children.forEach(n => {
+        targetNode.parent.addChild(n);
+      });
+      targetNode.parent.model.type = theOtherNode.model.type;
+      targetNode.parent.model.id = "branch";
+      targetNode.drop();
+      theOtherNode.drop();
+    } else {
+      if (targetNode.parent.isRoot()) {
+        targetNode.parent.model.type = null;
+        targetNode.parent.model.id = theOtherNode.model.id;
+        targetNode.drop();
+        theOtherNode.drop();
+      } else {
+        let grandParent = targetNode.parent.parent;
+        grandParent.addChild(theOtherNode);
+        targetNode.parent.drop();
+        targetNode.drop();
+      }
+    }
   }
 
-  _findNode(id){
+  _findNode(id) {
     let targetNode = this.root.first(node => {
       if (node.model.id === id) {
         return true;
       }
-    });  
-    return targetNode ? targetNode : null;  
+    });
+    return targetNode ? targetNode : null;
   }
 
   renderRecursively(node) {
@@ -112,11 +134,11 @@ class TileRoot extends Component<Props, State> {
       node.children.forEach(child => {
         if (node.model.type === "Col") {
           childViews.push(
-            <Col>{this.renderRecursively(child)}</Col>
+            <Col key={child.model.id}>{this.renderRecursively(child)}</Col>
           );
         } else if (node.model.type === "Row") {
           childViews.push(
-            <Row>{this.renderRecursively(child)}</Row>
+            <Row key={child.model.id}>{this.renderRecursively(child)}</Row>
           );
         }
       });
