2b80004da34ee0bd0f94579ddddfc809270ffef8
Shohei Kameda <shoheik@cpan.org>
2019-09-19 11:31:52 +0900

    can add same view

--- ios/Wazari/AppDelegate.m
+++ ios/Wazari/AppDelegate.m
@@ -19,9 +19,10 @@
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Wazari"
                                             initialProperties:nil];
-
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
+  [self setupScreenNotifications];
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
@@ -38,5 +39,51 @@
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+
+-(void)setupScreenNotifications {
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(externalScreenDidConnect:) name:UIScreenDidConnectNotification object:nil];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(externalScreenDidDisconnect:) name:UIScreenDidDisconnectNotification object:nil];
+  
+}
+
+-(void)externalScreenDidConnect:(NSNotification *)notification {
+  UIScreen *screen = (UIScreen *)[notification object];
+  if (screen != nil) {
+    [self setupExternalScreen:screen];
+  }
+}
+
+-(void)externalScreenDidDisconnect:(NSNotification *)notification {
+  id obj = [notification object];
+  if (obj != nil) {
+    [self teardownExternalScreen];
+  }
+}
+
+-(void)setupExternalScreen:(UIScreen *)screen {
+  
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:nil];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"Wazari"
+                                            initialProperties:nil];
+  
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  
+  self.externalWindow = [[UIWindow alloc]initWithFrame:screen.bounds];
+  self.externalWindow.rootViewController = rootViewController;
+  self.externalWindow.screen = screen;
+  self.externalWindow.hidden = false;
+}
+
+-(void)teardownExternalScreen {
+  if (self.externalWindow != nil) {
+    self.externalWindow.hidden = true;
+    self.externalWindow = nil;
+  }
+}
+
+
 
 @end
