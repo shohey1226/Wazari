bb7eed138cf7de62890b74a16d324a87525954fe
Shohei Kameda <shoheik@cpan.org>
2019-12-11 10:33:37 +0900

    added complicated object for keymap

--- app/components/WVInput.tsx
+++ app/components/WVInput.tsx
@@ -1,6 +1,135 @@
 import React, { Component } from "react";
 import { WebView } from "react-native-webview";
 
+// Use webview input
+class WVInput extends Component {
+  webref: WebView | null = null;
+
+  componentDidMount() {
+    const { modifiers, browserKeymap } = this.props;
+    this.setupKeymap(modifiers, browserKeymap);
+  }
+  componentWillUnmount() {
+    this.webref &&
+      this.webref.injectJavaScript(`document.getElementById('search').blur()`);
+  }
+
+  setupKeymap(modifiers, browserKeymap) {
+    // https://stackoverflow.com/questions/38750705/filter-object-properties-by-key-in-es6
+    const allowed = [
+      "home",
+      "end",
+      "deletePreviousChar",
+      "deleteNextChar",
+      "moveBackOneChar",
+      "moveForwardOneChar",
+      "deleteLine"
+    ];
+
+    const keymap = Object.keys(browserKeymap)
+      .filter(action => allowed.includes(action))
+      .reduce((obj, action) => {
+        const usedModifiers: Array<any> = Object.keys(
+          browserKeymap[action].modifiers
+        )
+          // only get true modifiers. e.g. ctrlKey: true
+          .filter(m => browserKeymap[action].modifiers[m]);
+
+        console.log(usedModifiers);
+        let replacedModifiers = usedModifiers.map(m => {
+          let modifierKeys: Array<string> = [];
+          Object.keys(modifiers).forEach(mf => {
+            if (modifiers[mf] === m) {
+              modifierKeys.push(mf);
+            }
+          });
+          return modifierKeys;
+        });
+
+        console.log(replacedModifiers);
+        this._allPossibleCases(replacedModifiers).forEach(modStr => {
+          let mods = modStr.split(",").map(m => {
+            switch (m) {
+              case "ctrlKey":
+                return "Control";
+              case "capslockKey":
+                return "CapsLock";
+              case "shiftKey":
+                return "Shift";
+              case "altKey":
+                return "Alt";
+              case "metaKey":
+                return "Meta";
+            }
+          });
+          console.log(mods);
+          if (!obj[action]) obj[action] = [];
+          obj[action].push({
+            keys: [browserKeymap[action].key, ...mods]
+          });
+        });
+        return obj;
+      }, {});
+    console.log(keymap);
+    const keymapStr = JSON.stringify(keymap);
+    setTimeout(() => {
+      this.webref &&
+        this.webref.injectJavaScript(`loadKeymaps('${keymapStr}')`);
+    }, 1000);
+  }
+
+  // https://stackoverflow.com/questions/4331092/finding-all-combinations-cartesian-product-of-javascript-array-values
+  _allPossibleCases(arr) {
+    if (arr.length == 1) {
+      return arr[0];
+    } else {
+      var result = [];
+      var allCasesOfRest = this._allPossibleCases(arr.slice(1)); // recur with the rest of array
+      for (var i = 0; i < allCasesOfRest.length; i++) {
+        for (var j = 0; j < arr[0].length; j++) {
+          result.push(arr[0][j] + "," + allCasesOfRest[i]);
+        }
+      }
+      return result;
+    }
+  }
+
+  onMessage(event) {
+    const data = JSON.parse(event.nativeEvent.data);
+    console.log(data);
+    switch (data.postFor) {
+      case "keyup":
+        this.props.keyup(data.inputValue);
+        break;
+      case "capsLock":
+        this.props.updateCapsLockState(data.capsLockOn);
+        break;
+      case "action":
+        this.props.updateAction(data.name);
+        break;
+    }
+  }
+
+  onLoadEnd() {
+    this.webref &&
+      this.webref.injectJavaScript(`document.getElementById('search').focus()`);
+  }
+
+  render() {
+    return (
+      <WebView
+        ref={r => (this.webref = r as any)}
+        originWhitelist={["*"]}
+        source={{ html: HTML }}
+        onLoadEnd={this.onLoadEnd.bind(this)}
+        onMessage={this.onMessage.bind(this)}
+      />
+    );
+  }
+}
+
+export default WVInput;
+
 const HTML = `
 <div id="container">
   <input type="text" id="search">
@@ -24,6 +153,8 @@ input[type="text"]:focus {
 }
 </style>
 <script>
+
+
 
 // https://stackoverflow.com/questions/5203407/how-to-detect-if-multiple-keys-are-pressed-at-once-using-javascript
 function Input(el){
@@ -114,90 +245,17 @@ function Input(el){
     return Input();
 }
 
-var search = Input(document.getElementById("search"));
-search.watch("escape", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "escape", postFor: "action"}));
-}, "Escape");
-search.watch("home", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "home", postFor: "action"}));
-}, "CapsLock", "m");
-search.watch("CapsLock", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "CapsLock", postFor: "action"}));
-}, "CapsLock");
-search.watch("Control", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "Control", postFor: "action"}));
-}, "Control");
-search.watch("Meta", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "Meta", postFor: "action"}));
-}, "Meta");
-search.watch("Alt", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "Alt", postFor: "action"}));
-}, "Alt");
-search.watch("alt+l", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "alt+l", postFor: "action"}));
-}, "Alt", "l");
-search.watch("control+l", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "control+l", postFor: "action"}));
-}, "Control", "l");
-search.watch("meta+l", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "meta+l", postFor: "action"}));
-}, "Meta", "l");
-search.watch("c", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "c", postFor: "action"}));
-}, "c");
-search.watch("d", function(map){
-  window.ReactNativeWebView.postMessage(JSON.stringify({name: "d", postFor: "action"}));
-}, "d");
-
+function loadKeymaps(keymapStr){
+  var search = Input(document.getElementById("search"));
+  var keymapObj = JSON.parse(keymapStr);
+  Object.keys(keymapObj).forEach((action) => {
+    keymapObj[action].forEach((h, index) => {
+      search.watch(action+index, function(map){
+        window.ReactNativeWebView.postMessage(JSON.stringify({name: action+index, postFor: "action"}));
+      }, ...h.keys);
+    })
+  });
+}
 
 </script>
 `;
-
-// Use webview input
-class WVInput extends Component {
-  webref: WebView | null = null;
-
-  componentDidMount() {
-    const { modifiers, browserKeymap } = this.props;
-    console.log(modifiers, browserKeymap);
-  }
-  componentWillUnmount() {
-    this.webref &&
-      this.webref.injectJavaScript(`document.getElementById('search').blur()`);
-  }
-
-  onMessage(event) {
-    const data = JSON.parse(event.nativeEvent.data);
-    console.log(data);
-    switch (data.postFor) {
-      case "keyup":
-        this.props.keyup(data.inputValue);
-        break;
-      case "capsLock":
-        this.props.updateCapsLockState(data.capsLockOn);
-        break;
-      case "action":
-        this.props.updateAction(data.name);
-        break;
-    }
-  }
-
-  onLoadEnd() {
-    this.webref &&
-      this.webref.injectJavaScript(`document.getElementById('search').focus()`);
-  }
-
-  render() {
-    return (
-      <WebView
-        ref={r => (this.webref = r as any)}
-        originWhitelist={["*"]}
-        source={{ html: HTML }}
-        onLoadEnd={this.onLoadEnd.bind(this)}
-        onMessage={this.onMessage.bind(this)}
-      />
-    );
-  }
-}
-
-export default WVInput;
